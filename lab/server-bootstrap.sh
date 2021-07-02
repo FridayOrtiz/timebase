ebbddf17b13f59af9937ef046822bc5ec3d6729a
Rafael Ortiz <rafael@ortiz.sh>
2021-07-01 22:17:39 -0400

    create lab environment
    We will need a laboratory environment to test our client and server software
    in. This establishes a client-server pair in the same virtual network, with
    bootstrap scripts to prepare everything both environments should need: Rust,
    Chrony, and some build + debugging tools.

--- lab/server-bootstrap.sh
+++ lab/server-bootstrap.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y build-essential clang llvm libclang-dev linux-tools-oem \
-  linux-tools-5.4.0-58-generic
+  linux-tools-5.4.0-58-generic chrony
 
 su vagrant << EOF
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -11,4 +11,11 @@ su vagrant << EOF
   source /home/vagrant/.cargo/env
   cargo install cargo-with
 EOF
+
+echo "192.168.5.3       tb-client" >> /etc/hosts
+echo "192.168.5.2       tb-server" >> /etc/hosts
+
+echo "allow 192.168.5.0/24" >> /etc/chrony/chrony.conf
+
+systemctl restart chrony.service
 
